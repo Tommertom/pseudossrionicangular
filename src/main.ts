0b07ab3860e03bcbeb202176919e18be7638a2eb
Tommertom <Tom.Gruintjes@gmail.com>
2022-08-06 22:25:37 +0200

    Fixing PWA load

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,30 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+if (environment.production) {
+  enableProdMode();
+}
+
 // eslint-disable-next-line @typescript-eslint/dot-notation
-window['bootAngularSPA'] = () => {
-  if (environment.production) {
-    enableProdMode();
-  }
+const bootFunction = () => {
 
   platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.log(err));
 };
+
+const isPWA = (win) =>
+  !!(win.matchMedia?.('(display-mode: standalone)').matches || (win.navigator).standalone);
+
+if (isPWA(window)) {
+  bootFunction();
+  // eslint-disable-next-line @typescript-eslint/dot-notation
+  window['bootAngularSPA'] = () => { };
+}
+
+if (!isPWA(window)) {
+  // eslint-disable-next-line @typescript-eslint/dot-notation
+  window['bootAngularSPA'] = bootFunction;
+}
+
+// eslint-disable-next-line @typescript-eslint/dot-notation
+console.log('asdasdadasdsssssssssa', window['bootAngularSPA'], window);
